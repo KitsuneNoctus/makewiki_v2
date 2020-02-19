a3fac250a2dc1bbcd9b87ef628e3de11d0717ce4
Henry Calderon <theceintiant@gmail.com>
2020-02-18 16:38:04 -0800

    Added new form to add a page. This works. Part two will be to make it so that user logged in is always the author and the writer can't just choose who.

--- wiki/views.py
+++ wiki/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
+from django.http import HttpResponseRedirect
+
 from wiki.models import Page
 
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+
+from wiki.forms import PageForm
 
 
 class SignUpView(CreateView):
@@ -36,3 +40,17 @@ class PageDetailView(DetailView):
         return render(request, 'page.html', {
           'page': page
         })
+
+
+# Stretch Challenge
+class PageCreateView(CreateView):
+  def get(self, request, *args, **kwargs):
+      context = {'form': PageForm()}
+      return render(request, 'new.html', context)
+
+  def post(self, request, *args, **kwargs):
+      form = PageForm(request.POST)
+      if form.is_valid():
+          page = form.save()
+          return HttpResponseRedirect(reverse_lazy('wiki:detail', args=[page.id]))
+      return render(request, 'new.html', {'form': form})
